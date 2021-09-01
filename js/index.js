35057b9bc26851c4306fa64659cb18185b3b3f6a
weiest <ImChiaWei93@gmail.com>
2021-09-01 22:12:31 +0800

    Prettier and added 404

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
         });
     });
 });
