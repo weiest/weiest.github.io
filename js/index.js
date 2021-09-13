f1163f01d8c6e35511781844729d2c4eb199fdbd
weiest <ImChiaWei93@gmail.com>
2021-09-14 02:45:18 +0800

    Random title with typewriting effect for introduction

--- js/index.js
+++ js/index.js
@@ -6,3 +6,77 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
         });
     });
 });
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+let TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    let elements = document.getElementsByClassName("typewrite");
+    for (let i = 0; i < elements.length; i++) {
+        let toRotate = elements[i].getAttribute("data-type");
+        let period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+            new TxtType(
+                elements[i],
+                JSON.parse(toRotate).sort(function (a, b) {
+                    return Math.random() - 0.5;
+                }),
+                period
+            );
+        }
+    }
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML =
+        ".typewrite > .wrap { padding-right:0.1em; border-right: 0.08em solid #000}";
+    document.body.appendChild(css);
+};
