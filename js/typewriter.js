723ee06301e9561da76193ad9c53263db2aa6ee5
weiest <ImChiaWei93@gmail.com>
2021-09-15 04:12:41 +0800

    cleanup and rename

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,12 +1,3 @@
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-
 let TxtType = function (el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -20,22 +11,14 @@ let TxtType = function (el, toRotate, period) {
 TxtType.prototype.tick = function () {
     let i = this.loopNum % this.toRotate.length;
     let fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
-
     let that = this;
     let delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
+    this.txt = fullTxt.substring(
+        0,
+        this.txt.length + (this.isDeleting ? -1 : 1)
+    );
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+    if (this.isDeleting) delta /= 2;
     if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
@@ -44,7 +27,6 @@ TxtType.prototype.tick = function () {
         this.loopNum++;
         delta = 500;
     }
-
     setTimeout(function () {
         that.tick();
     }, delta);
