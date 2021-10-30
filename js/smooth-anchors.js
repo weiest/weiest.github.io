044178be919296174fb4c86bab06632082ea885b
weiest <ImChiaWei93@gmail.com>
2021-10-30 19:57:37 +0800

    added dark mode and lint

--- js/smooth-anchors.js
+++ js/smooth-anchors.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth",
-        });
-    });
+	anchor.addEventListener("click", function (e) {
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+			behavior: "smooth",
+		});
+	});
 });
