551469f3ea86207cbe58b9ee51254fb15ee3b632
weiest <4051847+weiest@users.noreply.github.com>
2022-08-03 19:16:26 +0800

    Full redesign and repurpose of this repository.
    Decided to rebuild this into a "slides" style format used for interviews and introductions if needed.

--- js/common.js
+++
@@ -1,8 +0,0 @@
-const generateElement = (tag, text = null, className = null) => {
-	const element = document.createElement(tag);
-	if (text !== null) element.innerHTML = text;
-	if (className !== null) element.className = className;
-	return element;
-};
-
-export { generateElement };
