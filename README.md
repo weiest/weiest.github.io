c83034e17780774dc638c4032e150b88221d2eee
weiest <4051847+weiest@users.noreply.github.com>
2024-05-15 09:01:28 +0800

    Initial Revamp, old site in /old/

---
+++ README.md
@@ -0,0 +1,10 @@
+# weiest.github.io
+Hi! My name is Wei.
+This repository contains my portfolio that was built during my free time while also practicing my skills and trying new things out.
+
+**Credits**
+-   [Bitcoin timeline with fixed header using flexbox](https://codepen.io/mathiesjanssen/pen/ggeBKm "Bitcoin timeline with fixed header using flexbox") by [Matys](https://codepen.io/mathiesjanssen)
+-   [Photo by Markus Spiske](https://www.pexels.com/photo/coronavirus-statistics-on-screen-3970330/) from [Pexels](https://www.pexels.com/)
+-   [Photo by luis gomes](https://www.pexels.com/photo/close-up-photo-of-programming-of-codes-546819/) from [Pexels](https://www.pexels.com/)
+-   [Photo by Jakub Novacek](https://www.pexels.com/photo/time-lapse-photo-of-stars-on-night-924824/) from [Pexels](https://www.pexels.com/)
+-   [Bootstrap-dark-5](https://vinorodrigues.github.io/bootstrap-dark-5/)
---
+++ js/typewriter.js
@@ -0,0 +1,57 @@
+const DEFAULT_SPEED = 200;
+const DEFAULT_PERIOD = 2000;
+const DEFAULT_WAIT = 500;
+
+const random = function (a, b) {
+	return Math.random() - 0.5;
+};
+
+class TypeWriter {
+	constructor(element, contents, speed, period) {
+		this.contents = contents;
+		this.element = element;
+		this.speed = parseInt(speed, 10) || DEFAULT_SPEED;
+		this.period = parseInt(period, 10) || DEFAULT_PERIOD;
+		this.loopNum = 0;
+		this.txt = "";
+		this.isDeleting = false;
+		this.tick();
+	}
+	tick() {
+		let that = this;
+		let i = this.loopNum % this.contents.length;
+		let fullTxt = this.contents[i];
+		let delta = this.speed - Math.random() * 100;
+		this.txt = fullTxt.substring(
+			0,
+			this.txt.length + (this.isDeleting ? -1 : 1)
+		);
+		this.element.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+		if (this.isDeleting) delta /= 2;
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === "") {
+			this.isDeleting = false;
+			this.loopNum++;
+			delta = DEFAULT_WAIT;
+		}
+		setTimeout(function () {
+			that.tick();
+		}, delta);
+	}
+}
+
+window.onload = function () {
+	let elements = document.getElementsByClassName("typewriter");
+	for (let element of elements) {
+		let contents = element.getAttribute("data-contents");
+		let speed = element.getAttribute("data-speed");
+		let period = element.getAttribute("data-period");
+		let parsed_content = JSON.parse(contents);
+		if (element.hasAttribute("randomize")) parsed_content.sort(random);
+		if (contents) {
+			new TypeWriter(element, parsed_content, speed, period);
+		}
+	}
+};
